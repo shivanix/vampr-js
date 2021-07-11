06418baed95581b657f2f6a091fe1cf40b72b2f9
shivanix <shvni.here@gmail.com>
2021-07-11 23:36:42 +0000

    Added methods to vampire.js

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let thisVampire = this.numberOfVampiresFromOriginal;
+    let comparedVampire = vampire.numberOfVampiresFromOriginal;
+    return thisVampire < comparedVampire;
+   
+    //vampire with less numberofVampiresFrpmOriginal is more senior
   }
 
   /** Stretch **/
